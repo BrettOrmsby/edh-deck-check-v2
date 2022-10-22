2065f737564f9b721ae0aeba65ca8d683fbf6caf
BrettOrmsby <brettwormsby@outlook.com>
2022-10-22 10:59:02 -0400

    Revert "Build: Fix vercel build bug" and fix issue
    This reverts commit f4eebb5436348bece425fa6bb6371e2ef2513f1a.

--- src/components/ComboModal.vue
+++ src/components/ComboModal.vue
@@ -80,7 +80,7 @@ export default {
 
 <script lang="ts" setup>
 import Dialog from "primevue/dialog";
-import Button from "primevue/button/sfc";
+import Button from "primevue/button"
 import CardImage from "@/components/combo/CardImage.vue";
 import SymbolText from "@/components/combo/SymbolText.vue";
 import { computed } from "vue";
